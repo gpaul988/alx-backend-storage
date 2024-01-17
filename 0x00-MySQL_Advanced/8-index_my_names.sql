5dc0c379d6d525be4b30820a3e8d015e78a5112c
gpaul988 <gpaul988gmail.com>
2024-01-17 21:08:10 +0100

    fixed 8-index_my_names.sql

--- 0x00-MySQL_Advanced/8-index_my_names.sql
+++ 0x00-MySQL_Advanced/8-index_my_names.sql
@@ -1,2 +1,3 @@
--- Creates an index idx_name_first on the table names and the first letter of name.
+-- Generate indication idx_name_first on the table names and the first letter of name.
+-- Graham S. Paul - 8-index_my_names.sql
 CREATE INDEX idx_name_first ON names(name(1));

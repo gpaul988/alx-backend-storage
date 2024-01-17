feb0e3de0298fe5de340af6ba5008fe8d211032b
gpaul988 <gpaul988gmail.com>
2024-01-17 20:32:55 +0100

    fixed

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,5 +1,6 @@
--- Lists all bands with Glam rock as their main style, ranked by their longevity.
+-- File all bands with Glam rock as default style, ranked by their longevity.
 -- SELECT band_name, (IFNULL(split, YEAR(CURRENT_DATE())) - formed) AS lifespan
+-- Graham S. Paul - 3-glem-rock.sql
 SELECT band_name, (IFNULL(split, '2020') - formed) AS lifespan
     FROM metal_bands
     WHERE FIND_IN_SET('Glam rock', IFNULL(style, "")) > 0

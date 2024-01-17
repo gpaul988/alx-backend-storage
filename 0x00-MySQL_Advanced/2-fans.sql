17907c29071385faa817d2b739b0aadd1446734c
gpaul988 <gpaul988@gmail.com>
2024-01-17 11:26:24 -0800

    fixed

--- 0x00-MySQL_Advanced/2-fans.sql
+++ 0x00-MySQL_Advanced/2-fans.sql
@@ -1,4 +1,5 @@
--- Ranks country origins of bands, ordered by the number of (non-unique) fans.
+-- File country origins of bands, sorte by the number of (non-unique) fans.
+-- Graham S. Paul - 2-fans.sql
 SELECT origin, SUM(fans) AS nb_fans
     FROM metal_bands
     GROUP BY origin

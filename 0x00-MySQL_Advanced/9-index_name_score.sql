4e16b4e92c2c7ccc0f6d457251a6ece35b807ecd
gpaul988 <gpaul988gmail.com>
2024-01-17 21:10:20 +0100

    fixed 9-index_name_score.sql

--- 0x00-MySQL_Advanced/9-index_name_score.sql
+++ 0x00-MySQL_Advanced/9-index_name_score.sql
@@ -1,3 +1,3 @@
--- Creates an index idx_name_first_score on the table names and
--- the first letter of name and the score.
+-- Generates indication for  idx_name_first_score on the table names
+-- Graham S. Paul - 9-index_name_score.sql
 CREATE INDEX idx_name_first_score ON names(name(1), score);

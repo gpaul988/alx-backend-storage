06e231e964fc988b5916d7c6e37eb66575b36b4a
gpaul988 <gpaul988gmail.com>
2024-01-24 16:08:14 +0100

    bug removed

--- 0x01-NoSQL/12-log_stats.py
+++ 0x01-NoSQL/12-log_stats.py
@@ -12,15 +12,15 @@ def print_nginx_request_logs(nginx_collection):
     methods = ['GET', 'POST', 'PUT', 'PATCH', 'DELETE']
     for method in methods:
         req_count = len(list(nginx_collection.find({'method': method})))
-        print('\tmethod {}: {}'.format(method, req_count))
+        print(f'\tmethod {method}: {req_count}')
     status_checks_count = len(list(
         nginx_collection.find({'method': 'GET', 'path': '/status'})
     ))
-    print('{} status check'.format(status_checks_count))
+    print(f'{status_checks_count} status check')
 
 
 def run():
-    '''Provides some stats about Nginx logs stored in MongoDB.
+    '''gives some stats about Nginx logs stored in MongoDB.
     '''
     client = MongoClient('mongodb://127.0.0.1:27017')
     print_nginx_request_logs(client.logs.nginx)

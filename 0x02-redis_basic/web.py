00da68d4edb0576bd98d76d88598f0807bdf9085
gpaul988 <gpaul988gmail.com>
2024-01-24 17:38:41 +0100

    fixed

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -1,30 +1,21 @@
 #!/usr/bin/env python3
 """
-web cache and tracker
+create a web cach
 """
-import requests
 import redis
-from functools import wraps
+import requests
+rc = redis.Redis()
+count = 0
 
-store = redis.Redis()
+
+def get_page(url: str) -> str:
+    """ get a page and cach value"""
+    rc.set(f"cached:{url}", count)
+    resp = requests.get(url)
+    rc.incr(f"count:{url}")
+    rc.setex(f"cached:{url}", 10, rc.get(f"cached:{url}"))
+    return resp.text
 
 
-def count_url_access(method):
-    """ Decorator counting how many times
-    a URL is accessed """
-    @wraps(method)
-    def wrapper(url):
-        cached_key = f"cached:{url}"
-        cached_data = store.get(cached_key)
-        if cached_data:
-            return cached_data.decode("utf-8")
-
-        count_key = f"count:{url}"
-        html = method(url)
-
-        store.incr(count_key)
-        store.set(cached_key, html)
-        store.expire(cached_key, 10)
-        return html
-
-    return wrapper
+if __name__ == "__main__":
+    get_page('http://slowwly.robertomurray.co.uk')
